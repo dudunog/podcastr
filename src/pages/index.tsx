9fe1c8181f3269ecf3ff7a68be8920ce1c727062
Eduardo Nogueira <eduardodudunog@gmail.com>
2021-05-02 23:32:26 -0300

    I removed the api from the data. Now the data comes from the json file.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,7 +5,6 @@ import Link from "next/link";
 import { format, parseISO } from "date-fns";
 import ptBR from "date-fns/locale/pt-BR";
 
-import { api } from "../services/api";
 import { convertDurationToTimeString } from "../utils/convertDurationToTimeString";
 import { usePlayer } from "../contexts/PlayerContext";
 
@@ -129,17 +128,10 @@ export default function Home({ latestEpisodes, allEpisodes }: HomeProps) {
   );
 }
 
-export const getStaticProps: GetStaticProps = async () => {
-  //Biblioteca axios para fazer requisições HTTP
-  const { data } = await api.get("episodes", {
-    params: {
-      _limit: 12,
-      _sort: "published_at",
-      _order: "desc",
-    },
-  });
+const episodesJson = require("../../server.json");
 
-  const episodes = data.map((episode) => {
+export const getStaticProps: GetStaticProps = async () => {
+  const episodes = episodesJson.episodes.map((episode) => {
     return {
       id: episode.id,
       title: episode.title,
